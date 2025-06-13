e96880a3d3df33f8ca156ad7b3368de632593d3e
Kamruzzaman <kzamanbn@gmail.com>
2025-06-14 04:43:34 +0600

    refactor: reorganize index.tsx to use domReady for rendering and update package.json with author and dependencies

--- admin/index.tsx
+++ admin/index.tsx
@@ -2,12 +2,14 @@ import App from './App';
 import { createRoot } from '@wordpress/element';
 import activeMenu from './utils/menu';
 import './main.css';
+import domReady from '@wordpress/dom-ready';
 
+domReady(() => {
+    const container = document.getElementById('debug-suite-admin-app');
+    if (container) {
+        const root = createRoot(container);
+        root.render(<App />);
+    }
+});
 
-const container = document.getElementById('debug-suite-admin-app');
-
-if (container) {
-    activeMenu('debug-suite');
-    const root = createRoot(container);
-    root.render(<App />);
-}
+activeMenu('debug-suite');
