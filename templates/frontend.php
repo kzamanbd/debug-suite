ea5f7fc075d96657bbac9d283c203d8f3910c1b9
Kamruzzaman <kzamanbn@gmail.com>
2025-07-14 21:31:09 +0600

    Update frontend.php

--- templates/frontend.php
+++ templates/frontend.php
@@ -13,17 +13,20 @@ if ( ! wp_script_is( 'debug-suite-script', 'registered' ) || ! wp_style_is( 'deb
 }
 
 $debug_suite_settings = get_option( 'debug_suite_settings', [] );
-$debug_suite_settings = array_merge( $debug_suite_settings, [
-	'wpDebug'        => WP_DEBUG,
-	'wpDebugLog'     => WP_DEBUG_LOG,
-	'wpDebugDisplay' => WP_DEBUG_DISPLAY,
-	'publicRootPath' => ABSPATH,
-	'filesUrl'       => content_url(),
-	'favicon'        => DEBUG_SUITE_PLUGIN_URL . 'assets/images/brand-logo.png',
-	'wpVersion'      => get_bloginfo( 'version' ),
-	'phpVersion'     => phpversion(),
-	'isFrontend'     => true,
-] );
+$debug_suite_settings = array_merge(
+	$debug_suite_settings,
+	[
+		'wpDebug'        => WP_DEBUG,
+		'wpDebugLog'     => WP_DEBUG_LOG,
+		'wpDebugDisplay' => WP_DEBUG_DISPLAY,
+		'publicRootPath' => ABSPATH,
+		'filesUrl'       => content_url(),
+		'favicon'        => DEBUG_SUITE_PLUGIN_URL . 'assets/images/brand-logo.png',
+		'wpVersion'      => get_bloginfo( 'version' ),
+		'phpVersion'     => phpversion(),
+		'isFrontend'     => true,
+	]
+);
 
 // Ensure assets are enqueued
 wp_enqueue_script( 'debug-suite-script' );
