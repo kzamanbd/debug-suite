ec089da20f98ce9604cc2b831059af794aa8dfd0
Kamruzzaman <kzamanbn@gmail.com>
2025-07-30 20:35:27 +0600

    Update EmailLogServiceTest.php

--- tests/Unit/Services/EmailLog/EmailLogServiceTest.php
+++ tests/Unit/Services/EmailLog/EmailLogServiceTest.php
@@ -96,8 +96,7 @@ class EmailLogServiceTest extends DebugSuiteTestCase {
         $this->assert_service_result_success( $result );
         $this->assertArrayHasKey( 'entries', $result->get_data() );
         $this->assertArrayHasKey( 'total_count', $result->get_data() );
-        $this->assertArrayHasKey( 'current_page', $result->get_data() );
-        $this->assertArrayHasKey( 'total_pages', $result->get_data() );
+        $this->assertArrayHasKey( 'pagination', $result->get_data() );
     }
 
     /**
