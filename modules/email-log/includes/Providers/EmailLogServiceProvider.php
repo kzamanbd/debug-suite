83a78f1db017e51ea7def09028bf7037baecf54c
Kamruzzaman <kzamanbn@gmail.com>
2025-12-31 13:34:43 +0600

    Refactor Email Log module structure and registration

--- modules/email-log/includes/Providers/EmailLogServiceProvider.php
+++ modules/email-log/includes/Providers/EmailLogServiceProvider.php
@@ -4,16 +4,14 @@ namespace DebugSuite\Modules\EmailLog\Providers;
 
 use DebugSuite\Dependency\BaseServiceProvider;
 use DebugSuite\Modules\EmailLog\API\EmailLogController;
-use DebugSuite\Modules\EmailLog\HookRegistry;
-use DebugSuite\Modules\EmailLog\Assets;
+use DebugSuite\Modules\EmailLog\EmailLogPage;
 use DebugSuite\Modules\EmailLog\Services\EmailLogService;
 
 class EmailLogServiceProvider extends BaseServiceProvider {
 	protected array $tags = [ 'email-log-service' ];
 
 	protected array $services = [
-		Assets::class,
-		HookRegistry::class,
+		EmailLogPage::class,
 		EmailLogService::class,
 	];
 	public function register(): void {
